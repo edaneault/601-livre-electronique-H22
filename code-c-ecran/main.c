4aaf9272e3c6170538785c5b8abd7f5fa952db28
prof <>
2022-04-22 12:11:58 -0400

    Corriger main.c pour afficher texte sur 3 lignes

--- code-c-ecran/main.c
+++ code-c-ecran/main.c
@@ -9,29 +9,37 @@ int main(void)
         } else {
                 printf("Awww\n");
         }
+
         FILE *fp;
         char c;
         int n = 0;
-        fp = fopen("book_to_read.txt","r");
-        if(fp == NULL) {
-        perror("Error in opening file");
-        return(-1);
-      } do {
-      c = fgetc(fp);
-      if( feof(fp) ) {
-         break ;
-      }
-      printf("%c", c);
-      for(int a = 0; a < 1000; a++){
-      buffer_texte[a] = c;
-      }
-      } while(1);
 
-     fclose(fp);
-        wsClear();
-        for(int i = 0; i < 22; i++) {
-            wsDisplayText(0,i*28, buffer_texte, 100);
+        fp = fopen("book_to_read.txt","r");
+
+        if(fp == NULL) {
+		perror("Error in opening file");
+		return(-1);
+	}
+
+	for(int a = 0; a < 1000; a++) {
+	      c = fgetc(fp);
+	      if( feof(fp) ) {
+	           break ;
+	      }
+
+	      //printf("%c", c);
+
+	      buffer_texte[a] = c;
         }
+
+        fclose(fp);
+
+        wsClear();
+
+        wsDisplayText(10, 0, buffer_texte, 60);
+        wsDisplayText(10, 28, &buffer_texte[60], 60);
+        wsDisplayText(10, 56, &buffer_texte[120], 60);
+
         wsRefresh();
 
         return 0;
